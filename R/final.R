110a52ba713cd212e1c3b3615bc82dcaf8499912
Ben Elliott <ellio747@umn.edu>
2026-04-25 11:35:14 -0500

    setup R file, pre-analyses

--- R/final.R
+++ R/final.R
@@ -7,6 +7,9 @@ library(jsonlite)             # use of JSON functionality
 # Data Import and Cleaning
 final_tbl <- read_csv("../data/glassdoor_reviews.csv") # import dataset
 
+# save dataset
+write_rds(final_tbl, "../out/data.RDS") # saves final dataset as per line 3.3
+
 get_embedding <- function(text_strings) { #returns embedding vector for any string
   response <- POST(
     url = "http://localhost:11434/api/embed", # post to local Ollama server
@@ -20,6 +23,7 @@ get_embedding <- function(text_strings) { #returns embedding vector for any stri
   return(unlist(result$embeddings)) # provides a vector back
 }
 
+
 # vec <- get_embedding("test")
 # length(vec)
 
@@ -28,3 +32,10 @@ get_embedding <- function(text_strings) { #returns embedding vector for any stri
 # Analysis
 
 # Publication
+
+# RQ1. Does the use of embeddings (using the nomic-embed-text LLM embeddings model) improve prediction of satisfaction beyond a rigorous tokenization strategy?
+# RQ2. Does the use of topics improve prediction of satisfaction beyond a rigorous tokenization strategy?
+# RQ3. Does the use of embeddings plus topics improve prediction of satisfaction beyond either alone?
+# RQ4. What is the best prediction of overall job satisfaction achievable using text reviews as source data?
+
+# save.image(file = "../out/final.RData") #saves an .RData file as per line 3.4
